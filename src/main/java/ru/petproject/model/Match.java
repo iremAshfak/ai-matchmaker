1d580b8dfdc9468f4897bb83f4bc4cdd4627c0ed
Irem Ashfak <iashfak@yandex.ru>
2025-11-26 20:31:32 +0300

    Update Match.java

--- src/main/java/ru/petproject/model/Match.java
+++ src/main/java/ru/petproject/model/Match.java
@@ -4,7 +4,10 @@ import java.time.LocalDateTime;
 
 public class Match {
     private Long id;
+    
     private User user1;
+    
     private User user2;
+    
     private LocalDateTime matchingTime;
 }
