3b99e80ee2f8a805f76cfe6118fdb311272ff0e3
Irem Ashfak <iashfak@yandex.ru>
2025-11-28 18:54:57 +0300

    updated User

--- src/main/java/ru/petproject/model/User.java
+++ src/main/java/ru/petproject/model/User.java
@@ -5,10 +5,16 @@ import lombok.Data;
 @Data
 public class User {
     private Long id;
+
     private String name;
+
     private Integer age;
+
     private String description;
+
     private String city;
+
     private Double latitude;
+
     private Double longtitude;
 }
