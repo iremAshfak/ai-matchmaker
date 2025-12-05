2b1dbffdd3c038f7c628173f8254d4075fe5873f
Irem Ashfak <iashfak@yandex.ru>
2025-12-05 18:25:11 +0300

    add UpdateProfileRequest

---
+++ src/main/java/ru/petproject/dto/UpdateProfileRequest.java
@@ -0,0 +1,34 @@
+package ru.petproject.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import ru.petproject.model.enums.AuthRole;
+import ru.petproject.model.enums.Gender;
+
+@Data
+public class UpdateProfileRequest{
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String name;
+
+    @NotNull
+    private Gender gender;
+
+    private Gender preferredGender;
+
+    @Min(value = 18)
+    private Integer age;
+
+    @Size(max = 500)
+    private String description;
+
+    @DecimalMin(value = "-90.0")
+    @DecimalMax(value = "90.0")
+    private Double latitude;
+
+    @DecimalMin(value = "-180.0")
+    @DecimalMax(value = "180.0")
+    private Double longitude;
+
+    private AuthRole role;
+}
