ca7d3c2387336c2887b319ca82661d78fe12636f
Irem Ashfak <iashfak@yandex.ru>
2025-12-15 23:42:59 +0300

    update Match

--- src/main/java/ru/petproject/model/Match.java
+++ src/main/java/ru/petproject/model/Match.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -19,4 +20,18 @@ public class Match {
     
     private LocalDateTime matchingTime;
     private Boolean active = true;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Match match = (Match) o;
+        return Objects.equals(id, match.id) && Objects.equals(user1, match.user1)
+                && Objects.equals(user2, match.user2) && Objects.equals(matchingTime, match.matchingTime)
+                && Objects.equals(active, match.active);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user1, user2, matchingTime, active);
+    }
 }
