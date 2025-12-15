1422c4fa5e467711483123677bf4a2cada39e089
Irem Ashfak <iashfak@yandex.ru>
2025-12-15 23:43:44 +0300

    update Swipe

--- src/main/java/ru/petproject/model/Swipe.java
+++ src/main/java/ru/petproject/model/Swipe.java
@@ -3,6 +3,7 @@ package ru.petproject.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -20,4 +21,18 @@ public class Swipe {
     private Boolean swipeResult;
 
     private LocalDateTime swipeTime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Swipe swipe = (Swipe) o;
+        return Objects.equals(id, swipe.id) && Objects.equals(user1, swipe.user1)
+                && Objects.equals(user2, swipe.user2) && Objects.equals(swipeResult, swipe.swipeResult)
+                && Objects.equals(swipeTime, swipe.swipeTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user1, user2, swipeResult, swipeTime);
+    }
 }
