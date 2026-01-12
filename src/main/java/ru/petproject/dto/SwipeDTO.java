a19262359d29a404c57abb4f6cb3a89c26bbb132
Irem Ashfak <iashfak@yandex.ru>
2026-01-12 20:53:36 +0300

    update SwipeDTO

--- src/main/java/ru/petproject/dto/SwipeDTO.java
+++ src/main/java/ru/petproject/dto/SwipeDTO.java
@@ -1,15 +1,13 @@
 package ru.petproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.petproject.model.Swipe;
 
 @Data
 public class SwipeDTO {
     private Swipe swipe;
     private boolean isMatch;
-
-    public SwipeDTO (Swipe swipe, boolean isMatch) {
-        this.swipe = swipe;
-        this.isMatch = isMatch;
-    }
 }
