cada0357e110ecc80693458b7b42a369f818dc57
Irem Ashfak <iashfak@yandex.ru>
2026-01-29 20:56:11 +0300

    update UserRepository.java

--- src/main/java/ru/petproject/repository/UserRepository.java
+++ src/main/java/ru/petproject/repository/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.petproject.model.User;
 import ru.petproject.model.enums.Gender;
-
 import java.util.List;
 import java.util.Optional;
 
